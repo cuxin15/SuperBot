8df16a2ac1c087bb700eee7a9d2eeff523d45ce5
Trương A Xin <42485703+truongaxin123@users.noreply.github.com>
2019-11-07 00:08:21 +0700

    Cập nhật thêm tính năng tính toán cho bot

--- utility_functions.py
+++ utility_functions.py
@@ -1,6 +1,8 @@
 import unidecode
 
 # UTILITY FUNCTION
+
+
 def find(finder, iterable):
     for i in iterable:
         if finder(i) == True:
@@ -11,7 +13,7 @@ def filter(filtor, iterable=[], query=''):
     query = query.lower()
     query = unidecode.unidecode(query)
     query = ''.join(query.split(' '))
-    print('Query:',query)
+    print('Query:', query)
     out = []
     for i in iterable:
         if filtor(i, query='') == True:
@@ -21,16 +23,18 @@ def filter(filtor, iterable=[], query=''):
 
 def isUrlImageJPG(url, query=''):
     # ../url/image.jpg
-    print('isUrlImageJPG',query)
+    print('isUrlImageJPG', query)
     split_url = url.split('/')
     if '.jpg' in split_url[-1].lower():
         return True
+
 
 def isUrlImagePNG(url, query=''):
     # ../url/image.jpg
     split_url = url.split('/')
     if '.png' in split_url[-1].lower():
         return True
+
 
 def inQuery(url, query):
     print('inQuery', query)
@@ -42,14 +46,39 @@ def inQuery(url, query):
     if split_query == query:
         return True
 
-def iDontKnowItsName(url,query):
+
+def iDontKnowItsName(url, query):
     a = isUrlImagePNG(url, query)
-    b = inQuery(url,query)
-    print('a',a)
-    print('b',b)
-    return a==b
+    b = inQuery(url, query)
+    print('a', a)
+    print('b', b)
+    return a == b
+
+# STRING
+
 
 def toLowcase(string):
     return unidecode.unidecode(string).lower()
 
 
+# MATH
+def fac(x):
+    """Factorial"""
+    out = 1
+    lim = 1
+    while lim <= x:
+        out *= lim
+        lim += 1
+    return out
+
+
+def fibonacci(n):
+    """Flowers and something like that. Dãy Fibonacci là dãy vô hạn các số tự nhiên bắt đầu bằng hai phần tử 0 và 1 hoặc 1 và 1, các phần tử sau đó được thiết lập theo quy tắc mỗi phần tử luôn bằng tổng hai phần tử trước nó."""
+    from math import sqrt
+    return int(((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5)))
+
+if __name__ == '__main__':
+    x = fibonacci(10)
+    y = fac(0)
+
+    print(x, y)
--- xbot.py
+++ xbot.py
@@ -5,6 +5,8 @@ from os import listdir
 from os.path import isfile, join
 from utility_functions import *
 from dotenv import load_dotenv
+from math import sin, cos, tan, sqrt
+
 
 import os
 import random
@@ -15,6 +17,7 @@ import requests
 import json
 import unidecode
 import forecasts
+
 
 load_dotenv()
 # SETTING=========================================
@@ -36,7 +39,7 @@ async def on_ready():
             break
 
     print(f'{bot.user} da duoc ket noi thanh cong toi guild:\n'
-            f'{guild.name} (id: {guild.id})')
+          f'{guild.name} (id: {guild.id})')
 
     members = '\n - '.join([member.name for member in guild.members])
     print('Thành viên có trong guild:\n -', members)
@@ -46,8 +49,8 @@ async def on_ready():
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-            f'Chào {member.name}, chào mừng người anh em đến với Xóm trọ vui vẻ :)'
-        )
+        f'Chào {member.name}, chào mừng người anh em đến với Xóm trọ vui vẻ :)'
+    )
 
 
 @bot.command(name='search', help='Search something on wikipedia and attach an images')
@@ -56,16 +59,16 @@ async def search(ctx, *args):
     query = ' '.join(args)
     response = wikipedia.summary(query, sentences=2)
     await ctx.send(response)
-    
+
     # tải hình ảnh từ wikipedia
     # lấy ra một list các url ảnh phù hợp, sau đó chọn ngẫu nhiên một trong số chúng để đính kèm
     # ban đầu sẽ check xem thư mục chứa image có chứa ảnh có query hay không
     # nếu có dùng cái đó để tải lên
     # nếu không thì tải ảnh từ wiki xuống và tải lên#
-    
+
     # check in folder and get image to upload
     onlyfiles = [IMAGES_FOLDER+'/'+f for f in listdir(IMAGES_FOLDER)]
-    print("In local:",onlyfiles)
+    print("In local:", onlyfiles)
     list_image_paths = filter(inQuery, onlyfiles, query)
     print(list_image_paths)
     if list_image_paths != []:
@@ -89,8 +92,8 @@ async def search(ctx, *args):
             my_url = random.choice(list_image_urls)
 
             image_name = my_url.split('/')[-1]
-            path_to_image = IMAGES_FOLDER+'/'+query+'-'+image_name # query to mark image
-            urlretrieve(my_url,path_to_image) # download image
+            path_to_image = IMAGES_FOLDER+'/'+query+'-'+image_name  # query to mark image
+            urlretrieve(my_url, path_to_image)  # download image
 
             file = discord.File(path_to_image, filename="image.png")
             embed = discord.Embed()
@@ -101,16 +104,16 @@ async def search(ctx, *args):
 @bot.command(name='weather', help='Show weather information at a location')
 async def weather_current(ctx, *args):
     # args = ('Đà','Nẵng') => danang
-    args = map(toLowcase,args)
+    args = map(toLowcase, args)
     print(args)
     location = ' '.join(args)
-    print('location:',location)
-    with open('city.json','r') as f:
+    print('location:', location)
+    with open('city.json', 'r') as f:
         data = json.load(f)
     for i in data:
         if location in i['name'].lower():
-            print('id:',i['id'])
-            params = {'id':i['id'], 'appid':OPEN_WEATHER}
+            print('id:', i['id'])
+            params = {'id': i['id'], 'appid': OPEN_WEATHER}
             r = requests.get(
                 'http://api.openweathermap.org/data/2.5/forecast?', params)
             json_string = json.loads(r.text)
@@ -124,16 +127,30 @@ async def weather_forecasts(ctx, *args):
     # $weather_forecast Vinh Long
     location = ' '.join(args)
     im = forecasts.element_screenshot(location=location,
-                       apikey='0092e33dc81d0caadd56e552dacd5717',
-                       eleLongestHeight='/html/body/main',
-                       by1='xpath',
-                       eleScreenshot='container-openweathermap-widget-11',
-                       by2='id',
-                       img_name='weather.png')
+                                      apikey='0092e33dc81d0caadd56e552dacd5717',
+                                      eleLongestHeight='/html/body/main',
+                                      by1='xpath',
+                                      eleScreenshot='container-openweathermap-widget-11',
+                                      by2='id',
+                                      img_name='weather.png')
 
     file = discord.File('weather.png', filename="image.png")
     embed = discord.Embed()
     embed.set_image(url="attachment://image.png")
     await ctx.send(file=file, embed=embed)
+
+
+@bot.command(name='calculate', help='calculate math basic')
+async def calculate(ctx, *args):
+    funcs_dict = {'fac': fac, 'fib': fibonacci,
+                  'sin': sin, 'cos': cos, 'tan': tan, 'sqrt': sqrt}
+    result = eval(' '.join(args), funcs_dict)
+    reply = [
+        'Kết quả là ',
+        'Hình như là ',
+        'Em tính ra được ',
+        f'{" ".join(args)} = ',
+    ]
+    await ctx.send(random.choice(reply)+str(result))
 
 bot.run(TOKEN)
